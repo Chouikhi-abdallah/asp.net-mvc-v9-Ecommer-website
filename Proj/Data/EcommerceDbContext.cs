c8b66b9ba0c35684767bfcc04559b0120af3a07e
Chouikhi-abdallah <chouikhiabdallahpro@gmail.com>
2024-12-19 20:14:32 +0100

    added db context

--- Proj/Data/EcommerceDbContext.cs
+++ Proj/Data/EcommerceDbContext.cs
@@ -12,6 +12,10 @@ public class EcommerceDbContext : DbContext
     public DbSet<Cart> Carts { get; set; }
     public DbSet<Product> Products { get; set; }
     public DbSet<Category> Categories { get; set; }
+    
+    public DbSet<Order> Orders { get; set; }
+    
+    public DbSet<OrderItem> OrderItems { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
